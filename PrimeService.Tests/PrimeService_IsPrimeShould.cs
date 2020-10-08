949dce7152fb79fd43f48d55b24eae890e539dd8
@willhead <60652390+will-head@users.noreply.github.com>
2020-10-08 11:54:59 +0100

    Adds test for Less than 2

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -20,5 +20,16 @@ namespace Prime.UnitTests.Services
 
             Assert.IsFalse(result, "1 should not be prime");
         }
+
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
     }
 }
